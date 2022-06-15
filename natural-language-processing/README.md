c1cf625cebff4c73174cd4b9957d352276a3e6e2
MaximeDeloche <58418423+MaximeDeloche@users.noreply.github.com>
2022-06-15 17:37:36 +0200

    Add code for Transformer training

---
+++ natural-language-processing/README.md
@@ -0,0 +1,23 @@
+# Kubeflow on Power
+## NLP Demo
+
+Question Answering example, based on HuggingFace Transformers and trained of SQuAD dataset
+
+Sources: 
+
+* https://huggingface.co/docs/transformers/main/en/tasks/question_answering
+* https://colab.research.google.com/github/huggingface/notebooks/blob/main/examples/question_answering.ipynb
+
+
+### Environment setup
+
+```
+conda config --prepend channels https://ftp.osuosl.org/pub/open-ce/1.6.1/
+conda config --prepend channels huggingface
+
+conda create -n opence \
+	pytorch=1.10.2=cuda11.2_py39_1 \
+	datasets=2.3.1 \
+	transformers=4.18.0 \
+	onnxruntime=1.11.0
+```
---
+++ natural-language-processing/train_huggingface_transformer.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datasets import load_dataset
+from transformers import AutoTokenizer, DefaultDataCollator, \
+        AutoModelForQuestionAnswering, TrainingArguments, Trainer
+
+import transformers.onnx
+
+squad = load_dataset("squad")
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+def preprocess_function(examples):
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=384,
+        truncation="only_second",
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        answer = answers[i]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label it (0, 0)
+        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
+
+tokenized_squad = squad.map(preprocess_function, batched=True, remove_columns=squad["train"].column_names)
+
+data_collator = DefaultDataCollator()
+
+model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=1,#3,
+    weight_decay=0.01,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    # train_dataset=tokenized_squad["train"],
+    train_dataset=tokenized_squad["train"].select(range(1000)),
+    eval_dataset=tokenized_squad["validation"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
